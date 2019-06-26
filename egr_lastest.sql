ccb8ecb0e484c4685b4ee214a66f707c58c8adf5
Markus Mayr <markusmayr@gmx.net>
2019-06-26 15:11:40 +0200

    added joining of information lost after the merge

--- egr_lastest.sql
+++ egr_lastest.sql
@@ -1,16 +1,21 @@
 CREATE VIEW egr_latest AS
-SELECT st_union(the_geom), granule_id FROM (
+SELECT DISTINCT st_union(a.the_geom) as geom, b.granule_id, b.granule_time FROM (
 
 SELECT
   *,
   ROW_NUMBER() OVER w AS rnum
-FROM
-  egr_all
-WINDOW w AS (
-  PARTITION BY the_geom
-  ORDER BY granule_time DESC, granule_id DESC
-)
+  FROM
+    egr_all
+  WINDOW w AS (
+    PARTITION BY the_geom
+    ORDER BY granule_time DESC, granule_id DESC
+  )
+  ) a,
+  originalgranules b
 
-) t
-WHERE t.rnum = 1
-GROUP BY granule_id;
+WHERE
+    a.rnum = 1
+    AND a.location = b.location
+GROUP BY
+    b.granule_id,
+    b.granule_time;
